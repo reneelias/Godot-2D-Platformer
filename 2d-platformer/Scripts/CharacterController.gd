b79dab99220882ba1b4fa4897c71c871122ba013
Rene Elias <reneelias03@gmail.com>
2024-12-19 14:06:31 -0800

    Jump height precision controls

--- 2d-platformer/Scripts/CharacterController.gd
+++ 2d-platformer/Scripts/CharacterController.gd
@@ -6,7 +6,13 @@ class_name CharacterController
 @export var jumpSpeed := 100
 @export var friction := .025
 @export var inAirDamp := .5
+@export var jumpSpeedFrames := 10
+var jumpSpeedFramesCount := 0
 var startPos : Vector2
+
+@export var jumpSpeedScaler := .125
+# const JUMP_SPEED_SCALER = .125
+
 
 func _ready():
     startPos = global_position
@@ -22,10 +28,14 @@ func movement():
         velocity -= moveSpeedVec * (inAirDamp if (not is_on_floor()) else 1.0)
     if Input.is_key_pressed(KEY_D):
         velocity += moveSpeedVec * (inAirDamp if (not is_on_floor()) else 1.0)
-    if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
-        print("jumping")
-        velocity -= Vector2(0, jumpSpeed)
-
+    if Input.is_key_pressed(KEY_SPACE):
+        if is_on_floor():
+            jumpSpeedFramesCount = 0
+            velocity -= Vector2(0, jumpSpeed)
+        elif jumpSpeedFramesCount < jumpSpeedFrames:
+            jumpSpeedFramesCount += 1
+            velocity -= Vector2(0, jumpSpeed * jumpSpeedScaler * cos(float(jumpSpeedFramesCount)/jumpSpeedFrames * PI/2))
+            
     if is_on_floor():
         velocity -= Vector2(velocity.x * friction, 0)
 
