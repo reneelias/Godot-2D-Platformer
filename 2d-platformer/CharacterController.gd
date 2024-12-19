631c68409d86e7bac9af8185af8d37d51e37d181
Rene Elias <reneelias03@gmail.com>
2024-12-18 21:49:29 -0800

    Add jump + friction on floor

--- 2d-platformer/CharacterController.gd
+++ 2d-platformer/CharacterController.gd
@@ -1,7 +1,9 @@
 extends CharacterBody2D
 
-var gravityScale := 9.81
-var moveSpeed := 5.0
+@export var gravityScale := 9.81
+@export var moveSpeed := 5.0
+@export var jumpSpeed := 100
+@export var friction := .025
 
 func _physics_process(delta):
     velocity += Vector2(0, gravityScale)
@@ -10,5 +12,11 @@ func _physics_process(delta):
         velocity -= Vector2(moveSpeed, 0)
     if Input.is_key_pressed(KEY_D):
         velocity += Vector2(moveSpeed, 0)
+    if Input.is_key_pressed(KEY_SPACE) and is_on_floor():
+        print("jumping")
+        velocity -= Vector2(0, jumpSpeed)
+
+    if is_on_floor():
+        velocity -= Vector2(velocity.x * friction, 0)
         
     move_and_slide()
