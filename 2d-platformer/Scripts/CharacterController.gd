20f27f0a839ea5ba4ab17ed69dac3af8f4acc0d1
Rene Elias <reneelias03@gmail.com>
2024-12-23 21:45:12 -0800

    switch to Sprite2D + AnimationPlayer

--- 2d-platformer/Scripts/CharacterController.gd
+++ 2d-platformer/Scripts/CharacterController.gd
@@ -2,7 +2,8 @@ extends CharacterBody2D
 class_name CharacterController
 
 @export_group("References")
-@export var animSprite : AnimatedSprite2D
+@export var sprite2D : Sprite2D
+@export var animPlayer : AnimationPlayer
 
 @export_category("Physics")
 @export var gravityScale := 9.81
@@ -25,78 +26,78 @@ var startPos : Vector2
 var inputVelocity := Vector2.ZERO
 
 enum PlayerState {
-    IDLE,
-    RUN,
-    JUMP,
-    FALL
+	IDLE,
+	RUN,
+	JUMP,
+	FALL
 }
 var playerState := PlayerState.IDLE
 
 func _ready():
-    startPos = global_position
+	startPos = global_position
 
 func _physics_process(delta):
-    movement()
-    updatePlayerState()
+	movement()
+	updatePlayerState()
 
 func updatePlayerState():
-    if is_on_floor():
-        if inputVelocity.x != 0:
-            playerState = PlayerState.RUN
-        else:
-            playerState = PlayerState.IDLE
-    elif not is_on_floor() and velocity.y > 0:
-        playerState = PlayerState.FALL
+	if is_on_floor():
+		if inputVelocity.x != 0:
+			playerState = PlayerState.RUN
+		else:
+			playerState = PlayerState.IDLE
+	elif not is_on_floor() and velocity.y > 0:
+		playerState = PlayerState.FALL
 
 func movement():
-    velocity += Vector2(0, gravityScale)
-    inputVelocity = Vector2.ZERO
+	velocity += Vector2(0, gravityScale)
+	inputVelocity = Vector2.ZERO
 
-    var moveSpeedVec := Vector2(moveSpeed, 0)
-    if Input.is_key_pressed(KEY_A):
-        inputVelocity -= moveSpeedVec * (inAirDamp if (not is_on_floor()) else 1.0)
-    if Input.is_key_pressed(KEY_D):
-        inputVelocity += moveSpeedVec * (inAirDamp if (not is_on_floor()) else 1.0)
-        
-    if is_on_floor() and inputVelocity.length() == 0:
-        velocity -= Vector2(velocity.x * friction, 0)
-    
-    velocity += inputVelocity
-    if abs(velocity.x) > maxVelX:
-        velocity.x = maxVelX if velocity.x > 0 else -maxVelX
-    
-    if inputVelocity.x != 0:
-        animSprite.flip_h = inputVelocity.x < 0
-    
-    updateJump()
-            
-    if Input.is_key_pressed(KEY_R):
-        global_position = startPos
-        velocity = Vector2.ZERO
-    
-    move_and_slide()
+	var moveSpeedVec := Vector2(moveSpeed, 0)
+	if Input.is_key_pressed(KEY_A):
+		inputVelocity -= moveSpeedVec * (inAirDamp if (not is_on_floor()) else 1.0)
+	if Input.is_key_pressed(KEY_D):
+		inputVelocity += moveSpeedVec * (inAirDamp if (not is_on_floor()) else 1.0)
+		
+	if is_on_floor() and inputVelocity.length() == 0:
+		velocity -= Vector2(velocity.x * friction, 0)
+	
+	velocity += inputVelocity
+	if abs(velocity.x) > maxVelX:
+		velocity.x = maxVelX if velocity.x > 0 else -maxVelX
+	
+	if inputVelocity.x != 0:
+		sprite2D.flip_h = inputVelocity.x < 0
+	
+	updateJump()
+			
+	if Input.is_key_pressed(KEY_R):
+		global_position = startPos
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
 
 func updateJump():
-    if is_on_floor():
-        coyoteFramesCount = 0
-    elif coyoteFramesCount < coyoteFrames:
-        coyoteFramesCount += 1
-    
-    if Input.is_action_just_pressed("Jump") and playerState != PlayerState.JUMP:
-        if is_on_floor() or coyoteFramesCount < coyoteFrames:
-            jumpSpeedFramesCount = 0
-            jumpSpeedFrames = JUMP_FRAMES
-            velocity -= Vector2(0, jumpSpeed)
-            playerState = PlayerState.JUMP
-        elif is_on_wall() and get_wall_normal().x == -inputVelocity.normalized().x:
-            jumpSpeedFramesCount = 0
-            jumpSpeedFrames = WALL_JUMP_FRAMES
-            var wallJumpVec = get_wall_normal()
-            wallJumpVec = wallJumpVec.rotated(deg_to_rad(50) * wallJumpVec.x)
-            wallJumpVec.y = -abs(wallJumpVec.y)
-            velocity = wallJumpVec * wallJumpSpeed
-            playerState = PlayerState.JUMP
-    elif Input.is_action_pressed("Jump") and playerState == PlayerState.JUMP and jumpSpeedFramesCount < jumpSpeedFrames:
-        jumpSpeedFramesCount += 1
-        velocity -= Vector2(0, jumpSpeed * jumpSpeedScaler * cos(float(jumpSpeedFramesCount)/jumpSpeedFrames * PI/2))
-        # print("Jump Frames: ", jumpSpeedFramesCount)
+	if is_on_floor():
+		coyoteFramesCount = 0
+	elif coyoteFramesCount < coyoteFrames:
+		coyoteFramesCount += 1
+	
+	if Input.is_action_just_pressed("Jump") and playerState != PlayerState.JUMP:
+		if is_on_floor() or coyoteFramesCount < coyoteFrames:
+			jumpSpeedFramesCount = 0
+			jumpSpeedFrames = JUMP_FRAMES
+			velocity -= Vector2(0, jumpSpeed)
+			playerState = PlayerState.JUMP
+		elif is_on_wall() and get_wall_normal().x == -inputVelocity.normalized().x:
+			jumpSpeedFramesCount = 0
+			jumpSpeedFrames = WALL_JUMP_FRAMES
+			var wallJumpVec = get_wall_normal()
+			wallJumpVec = wallJumpVec.rotated(deg_to_rad(50) * wallJumpVec.x)
+			wallJumpVec.y = -abs(wallJumpVec.y)
+			velocity = wallJumpVec * wallJumpSpeed
+			playerState = PlayerState.JUMP
+	elif Input.is_action_pressed("Jump") and playerState == PlayerState.JUMP and jumpSpeedFramesCount < jumpSpeedFrames:
+		jumpSpeedFramesCount += 1
+		velocity -= Vector2(0, jumpSpeed * jumpSpeedScaler * cos(float(jumpSpeedFramesCount)/jumpSpeedFrames * PI/2))
+		# print("Jump Frames: ", jumpSpeedFramesCount)
