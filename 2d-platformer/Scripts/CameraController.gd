2fc24944b97f5e8759aae29eec6d03e53e53e40d
Rene Elias <reneelias03@gmail.com>
2025-01-24 10:50:45 -0800

    Cam2D now separate scene from Player

--- 2d-platformer/Scripts/CameraController.gd
+++ 2d-platformer/Scripts/CameraController.gd
@@ -1,2 +1,9 @@
 extends Camera2D
 class_name CameraController
+
+@export var objectToFollow : Node2D
+
+func _process(delta):
+    if objectToFollow:
+        global_position = objectToFollow.global_position + offset
+
