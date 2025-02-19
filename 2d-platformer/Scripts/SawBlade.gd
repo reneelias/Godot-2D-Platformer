968b80c23f9cb6b2da39eb201f315b717953833b
Rene Elias <reneelias03@gmail.com>
2025-02-18 21:42:23 -0800

    SawBlades can follow path + path/blades system in level

--- 2d-platformer/Scripts/SawBlade.gd
+++ 2d-platformer/Scripts/SawBlade.gd
@@ -1,13 +1,22 @@
 extends Area2D
 
+@export_category("References")
+@export var pathFollow : PathFollow2D
+
+@export_category("Settings")
 @export var spinSpeed := -15.0
 @export var invertSpin := false
+@export var followsPath := true
+@export var pathFollowSpeed := 70.0
 
 
 func _physics_process(delta):
-    rotate(spinSpeed * delta * (-1 if invertSpin else 1))
+	rotate(spinSpeed * delta * (-1 if invertSpin else 1))
+	if followsPath:
+		pathFollow.progress += delta * pathFollowSpeed
+		global_position = pathFollow.global_position
 
 func onBodyEntered(body: Node):
-    var player = body as CharacterController
-    if player:
-        player.death()
+	var player = body as CharacterController
+	if player:
+		player.death()
